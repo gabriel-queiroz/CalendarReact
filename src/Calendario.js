58e047823eed5cdc05c322d7647bd2459e3347cb
Gabriel Queiróz <gabrielqueirozzn@gmail.com>
2018-07-26 14:05:46 -0300

    Corrigir erro na date e adicionar tabela do calendário

--- src/Calendario.js
+++ src/Calendario.js
@@ -12,6 +12,10 @@ export default class Calendario extends React.Component {
 
 
     getDaysArray(year, month) {
+
+        console.log(year);
+        console.log(month);
+
         let names = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'];
         let date = new Date(year, month - 1, 1);
         let result = [];
@@ -50,19 +54,43 @@ export default class Calendario extends React.Component {
 
     renderCalendario() {
 
-        let year = this.state.currenteDate.format('MM');
-
-
-
+        let year = this.state.currenteDate.format('YYYY');
         let month = '';
         month = this.state.currenteDate.format('MM');
 
         let datas = JSON.stringify(this.getDaysArray(year, month));
 
         return (
+
             <div>
-                <h1>{`${this.state.currenteDate.format('MMM')}/${this.state.currenteDate.format('YYYY')}`}</h1>
-                <p> {datas} </p>
+                <table  style={{margin:'0 auto', border:'1px'}}>
+                    <thead>
+                        <th>Domingo</th>
+                        <th>Segunda</th>
+                        <th>Terça</th>
+                        <th>Quarta</th>
+                        <th>Quinta</th>
+                        <th>Sexta</th>
+                        <th>Sábado</th>
+                    </thead>
+
+                    <tbody>
+
+                                <td>1</td>
+                                <td>2</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>34</td>
+                                <td>4</td>
+                                <td>44</td>
+
+                    </tbody>
+
+                </table>
+                <div>
+                    <h1>{`${this.state.currenteDate.format('MMM')}/${this.state.currenteDate.format('YYYY')}`}</h1>
+                    <p> {datas} </p>
+                </div>
             </div>
         );
     }
